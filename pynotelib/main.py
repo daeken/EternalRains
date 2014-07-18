060ec97b87b8c3c956b6c1b47c941528791b5804
Cody Brocious <cody.brocious@gmail.com>
2014-07-17 20:13:11 -0400

    Initial harness for pynotelib

---
+++ pynotelib/main.py
@@ -0,0 +1,15 @@
+import notelib
+
+tests = (
+	('low_e_open', ('E2', )), 
+	('a_open', ('A2', )), 
+	('d_open', ('D3', )), 
+	('g_open', ('G3', )), 
+	('b_open', ('B3', )), 
+	('high_e_open', ('E4', )), 
+	('c_chord', ('E2', 'C2', 'E3', 'G3', 'C3', 'E4')), 
+	('e_chord', ('E2', 'B2', 'E3', 'G#3', 'B3', 'E4')), 
+)
+
+for fn, notes in tests:
+	print fn, notes, notelib.listen(fn, notes)
---
+++ pynotelib/notelib.py
@@ -0,0 +1,2 @@
+def listen(fn, notes):
+	pass
